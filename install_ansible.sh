019edf8b64d05709d232cdddc0bd4a14d562eb92
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-12-20 18:51:43 +0530

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,5 +3,5 @@ sudo yum -y install epel-release
 sudo yum -y install ansible
 sudo yum -y install git
 git clone https://github.com/muralialakuntla3/terraform-aws.git /home/centos
-cd tf-multiple-mdp
+cd terraform-aws
 ansible-playbook setup_ecomm.yml -i aws.ini
